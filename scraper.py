a9a11825ef6586d5e07996fb1cd4a2cf48625c8b
Lucas Môra <mora.lucas@live.com>
2020-11-27 07:24:42 -0300

    Consertado #1
    - Removidos imports não utilizados
    - Alterado número de páginas a serem lidas
    - Removido tempo de 1 segundo entre a leitura de cada página
    - O navegador é fechado antes do processamento dos dados pelo Python
    - Adicionados comentários extras

--- scraper.py
+++ scraper.py
@@ -1,15 +1,12 @@
-import sys
-import getopt
 import time
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from bs4 import BeautifulSoup
 import pandas as pd
-import re
 
 site = 'https://www.kabum.com.br/'
 termo_pesquisa = 'Memoria RAM 8gb'
-n_pgs = 1  # Número de páginas a serem lidas
+n_pgs = 5  # Número de páginas a serem lidas
 nome_arquivo = "memoria_ram_8gb.csv"  # Nome do arquivo a ser exportado
 
 # Configurando o webdriver e inserindo o termo de busca (Memória RAM 8gb)
@@ -23,12 +20,6 @@ campo_busca.send_keys(Keys.ENTER)
 # Aguarda 1 segundo para completar o carregamento
 time.sleep(1)
 
-# Inicia a extração das informações
-html = nav.find_element_by_id("listagem-produtos")
-html = html.get_attribute("innerHTML")
-
-sopa = BeautifulSoup(html, 'lxml')
-
 # Cada atributo será armazenado numa lista correspondente
 titulos = []
 precos_boleto = []
@@ -40,9 +31,13 @@ avaliacoes = []
 p = 1
 while True:
     try:
-        time.sleep(1)
-
         print(f"Lendo página {p}...")
+
+        # Inicia a extração das informações
+        html = nav.find_element_by_id("listagem-produtos")
+        html = html.get_attribute("innerHTML")
+
+        sopa = BeautifulSoup(html, 'lxml')
         
         for i in sopa.find_all('div', {'class': 'eITELq'}):
             titulo = i.find('a', {'class': 'item-nome'}).text
@@ -68,22 +63,27 @@ while True:
         print("Próxima página não encontrada. Continuando...")
         break
 
+# Fechando o webdriver
+nav.close()
+
 # Criando o DataFrame com as listas
 df = pd.DataFrame(columns=["Nome", "Preco_boleto", "Preco_prazo", "Estrelas", "Avaliacoes"])
 df["Nome"] = titulos
 
+# Formatando os separadores de decimais
 df["Preco_boleto"] = precos_boleto
 df["Preco_boleto"] = df["Preco_boleto"].apply(lambda x: x.split(' ')[1].replace(',', '.'))
 df["Preco_boleto"] = df["Preco_boleto"].astype(float)
-
 df["Preco_prazo"] = precos_prazo
-df["Preco_prazo"] = df["Preco_prazo"].apply(lambda x: x.split(' ')[1].replace(',', '.'))
+df["Preco_prazo"] = df["Preco_prazo"].apply(lambda x: x.split(' ')[1].replace('.', '').replace(',', '.'))
 df["Preco_prazo"] = df["Preco_prazo"].astype(float)
 
+# Formatando o campo Estrelas: extraindo somente o número
 df["Estrelas"] = estrelas
 df["Estrelas"] = df["Estrelas"].str.replace('e', '')
 df["Estrelas"] = df["Estrelas"].astype(int)
 
+# Extraindo apenas a quantidade de avaliações do campo Avaliações
 df["Avaliacoes"] = avaliacoes
 df["Avaliacoes"] = df.Avaliacoes.str.extract(r"(\d+)")
 df["Avaliacoes"] = df["Avaliacoes"].astype(int)
@@ -93,6 +93,3 @@ print(f"\n{len(df)} registros adicionados.")
 # Exportando para CSV
 df.to_csv(nome_arquivo, index=False)
 print(f"Arquivo salvo como {nome_arquivo}")
-
-# Fechando o webdriver
-nav.close()
